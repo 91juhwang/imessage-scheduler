691dff5d426cca040298ef803695958ea99e231e
James Hwang <91juhwang@gmail.com>
2026-01-09 22:17:25 -0600

    Add shadcn components

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-lg border-indigo-500/20",
         className
       )}
       {...props}
