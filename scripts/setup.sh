5d183d8026b7469b35389c4162253f5b690a2414
James Hwang <91juhwang@gmail.com>
2026-01-10 14:37:30 -0600

    Fix node version to v22.13, so that it matches correctly between web and gateway, adjust the setup.sh script to fail fast if wrong version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+REQUIRED_NODE_MAJOR=22
+CURRENT_NODE_MAJOR=$(node -p "process.versions.node.split('.')[0]")
+if [ "$CURRENT_NODE_MAJOR" -ne "$REQUIRED_NODE_MAJOR" ]; then
+  echo "Node ${REQUIRED_NODE_MAJOR}.x is required. You are running $(node -v)."
+  echo "Use nvm: nvm install ${REQUIRED_NODE_MAJOR} && nvm use ${REQUIRED_NODE_MAJOR}"
+  exit 1
+fi
+
 echo "Installing root dependencies..."
 pnpm -w install
 
